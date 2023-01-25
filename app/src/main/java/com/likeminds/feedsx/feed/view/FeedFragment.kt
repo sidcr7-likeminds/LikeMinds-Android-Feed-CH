d50f13e91c0d0b2d2a039df89cb448caa6a1cf4b
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-01-25 18:36:16 +0530

    create a sample for all posts screen

--- app/src/main/java/com/likeminds/feedsx/feed/view/FeedFragment.kt
+++ app/src/main/java/com/likeminds/feedsx/feed/view/FeedFragment.kt
@@ -7,7 +7,7 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.likeminds.feedsx.databinding.FragmentFeedBinding
 import com.likeminds.feedsx.feed.viewmodel.FeedViewModel
-import com.likeminds.feedsx.posttypes.model.PostViewData
+import com.likeminds.feedsx.posttypes.model.*
 import com.likeminds.feedsx.posttypes.view.adapter.PostAdapter
 import com.likeminds.feedsx.posttypes.view.adapter.PostAdapter.PostAdapterListener
 import com.likeminds.feedsx.utils.ViewUtils
@@ -47,6 +47,76 @@ class FeedFragment :
             adapter = mPostAdapter
             show()
         }
+
+        //TODO: Testing data
+        var text =
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        mPostAdapter.add(
+            PostViewData.Builder()
+                .id("1")
+                .user(UserViewData.Builder().name("Sid").customTitle("Admin").build())
+                .text(text)
+                .build()
+        )
+        text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
+        mPostAdapter.add(
+            PostViewData.Builder()
+                .id("2")
+                .user(UserViewData.Builder().name("Ishaan").customTitle("Admin").build())
+                .text(text)
+                .build()
+        )
+        text = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+        mPostAdapter.add(
+            PostViewData.Builder()
+                .id("3")
+                .user(UserViewData.Builder().name("Natesh").customTitle("Admin").build())
+                .text(text)
+                .build()
+        )
+        text = "Hello!"
+        mPostAdapter.add(
+            PostViewData.Builder()
+                .attachments(
+                    listOf(
+                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
+                            .build()
+                    )
+                )
+                .id("4")
+                .user(UserViewData.Builder().name("Mahir").customTitle("Admin").build())
+                .text(text)
+                .build()
+        )
+        mPostAdapter.add(
+            PostViewData.Builder()
+                .attachments(
+                    listOf(
+                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(VIDEO).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
+                            .build(),
+                        AttachmentViewData.Builder().fileType(VIDEO).fileUrl("").fileSize("")
+                            .build()
+                    )
+                )
+                .id("5")
+                .user(UserViewData.Builder().name("Natesh").customTitle("Admin").build())
+                .text(text)
+                .build()
+        )
     }
 
     private fun initToolbar() {
