0b18a691afa9281dee2905741b35c0d86acad058
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-01-30 16:59:28 +0530

    updated sample

--- app/src/main/java/com/likeminds/feedsx/feed/view/FeedFragment.kt
+++ app/src/main/java/com/likeminds/feedsx/feed/view/FeedFragment.kt
@@ -54,8 +54,8 @@ class FeedFragment :
         }
 
         //TODO: Testing data
-        var text =
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        val text =
+            "My name is Siddharth Dubey ajksfbajshdbfjakshdfvajhskdfv kahsgdv hsdafkgv ahskdfgv b "
         mPostAdapter.add(
             PostViewData.Builder()
                 .id("1")
@@ -63,8 +63,6 @@ class FeedFragment :
                 .text(text)
                 .build()
         )
-        text =
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
         mPostAdapter.add(
             PostViewData.Builder()
                 .id("2")
@@ -72,8 +70,6 @@ class FeedFragment :
                 .text(text)
                 .build()
         )
-        text =
-            "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
         mPostAdapter.add(
             PostViewData.Builder()
                 .id("3")
@@ -81,21 +77,24 @@ class FeedFragment :
                 .text(text)
                 .build()
         )
-        text = "Hello!"
         mPostAdapter.add(
             PostViewData.Builder()
                 .attachments(
                     listOf(
-                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(DOCUMENT).fileUrl("").fileSize("")
-                            .build()
+                        AttachmentViewData.Builder()
+                            .attachmentType(LINK)
+                            .attachmentMeta(
+                                AttachmentMetaViewData.Builder()
+                                    .ogTags(
+                                        LinkOGTags.Builder()
+                                            .title("Youtube video")
+                                            .image("https://i.ytimg.com/vi/EbyAoYaUcVo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDiI5bXtT71sC4IAnHiDAh52LxbFA")
+                                            .url("https://www.youtube.com/watch?v=sAuQjwEl-Bo")
+                                            .description("This is a youtube video")
+                                            .build()
+                                    )
+                                    .build()
+                            ).build()
                     )
                 )
                 .id("4")
@@ -107,15 +106,13 @@ class FeedFragment :
             PostViewData.Builder()
                 .attachments(
                     listOf(
-                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(VIDEO).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(IMAGE).fileUrl("").fileSize("")
-                            .build(),
-                        AttachmentViewData.Builder().fileType(VIDEO).fileUrl("").fileSize("")
+                        AttachmentViewData.Builder()
+                            .attachmentType(IMAGE)
+                            .attachmentMeta(
+                                AttachmentMetaViewData.Builder()
+                                    .url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKEHgSbU751Z6Gn5FsbVMw7x_VFyKGwwHEEUiC9HtnKw&s")
+                                    .build()
+                            )
                             .build()
                     )
                 )
