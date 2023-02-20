62c1ca5c49208cfcd552497e5a9eadf64d4f9b4f
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-02-20 16:21:20 +0530

    removed unused import

--- app/src/main/java/com/likeminds/feedsx/utils/customview/BaseDialogFragment.kt
+++ app/src/main/java/com/likeminds/feedsx/utils/customview/BaseDialogFragment.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
 import androidx.viewbinding.ViewBinding
