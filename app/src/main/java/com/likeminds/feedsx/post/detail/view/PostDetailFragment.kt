fa5b7a6d05566ac95b4854f877a8af3a60a18e85
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-04-12 13:30:46 +0530

    fixed bugs

--- app/src/main/java/com/likeminds/feedsx/post/detail/view/PostDetailFragment.kt
+++ app/src/main/java/com/likeminds/feedsx/post/detail/view/PostDetailFragment.kt
@@ -131,9 +131,11 @@ class PostDetailFragment :
     }
 
     // fetches post data to set initial data
-    private fun fetchPostData() {
-        // show progress bar
-        ProgressHelper.showProgress(binding.progressBar)
+    private fun fetchPostData(fromRefresh: Boolean = false) {
+        if (!fromRefresh) {
+            // show progress bar
+            ProgressHelper.showProgress(binding.progressBar)
+        }
         //if source is notification, then call initiate first and then other apis
         if (postDetailExtras.source == LMAnalytics.Source.NOTIFICATION) {
             initiateViewModel.initiateUser()
@@ -792,7 +794,7 @@ class PostDetailFragment :
     private fun refreshPostData() {
         mSwipeRefreshLayout.isRefreshing = true
         mScrollListener.resetData()
-        fetchPostData()
+        fetchPostData(true)
     }
 
     override fun likePost(position: Int) {
